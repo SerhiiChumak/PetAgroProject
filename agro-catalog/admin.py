21df902f29cd17bc31bcb8a0c87bcd79772c9c62
Serhii Chumak <s4umak@gmail.com>
2025-11-14 16:13:19 +0200

    Add admin.py and db.sqlite3

--- agro-catalog/admin.py
+++ agro-catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Culture, Disease, Drug
 
-# Register your models here.
+
+admin.site.register(Culture)
+admin.site.register(Disease)
+admin.site.register(Drug)
