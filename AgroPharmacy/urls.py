f7ec8a00f89b7748dd6592f3f52e7389f012cd61
Serhii Chumak <s4umak@gmail.com>
2025-11-18 11:55:48 +0200

    Modified: login, logout and base.html

--- AgroPharmacy/urls.py
+++ AgroPharmacy/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('agro_catalog.urls')),
     path('', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
