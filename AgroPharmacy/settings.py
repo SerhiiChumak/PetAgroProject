c8387df4ee0208e7d2badef14c59a84e668a2f95
Serhii Chumak <s4umak@gmail.com>
2025-11-19 10:44:29 +0200

    Add modified gitignore

--- AgroPharmacy/settings.py
+++ AgroPharmacy/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-88r+l!fxp!001l4^bh#kc@qx5$1@6gbi^2agws_3zp3z%o&0*%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
